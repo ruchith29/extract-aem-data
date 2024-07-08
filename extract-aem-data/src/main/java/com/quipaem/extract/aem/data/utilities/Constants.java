c0e8aa924d14a155b6853c69aa15f949a98e7665
ruchith29 <167510345+ruchith29@users.noreply.github.com>
2024-07-08 14:22:30 +0530

    Delete extract-aem-data directory

--- extract-aem-data/src/main/java/com/quipaem/extract/aem/data/utilities/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.quipaem.extract.aem.data.utilities;
-
-public class Constants {
-    public final static String USERNAME="quip-service-user";
-    public final static String PASSWORD="quipServiceUser";
-    public static String AEM_DATA_NAME= "seoPage";
-    public static String COLLECTION_NAME="seoPageData";
-}
