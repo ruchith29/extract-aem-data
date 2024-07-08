4523d04798d2b087fdfc7b8c9f603a641ed7ea96
ruchith29 <167510345+ruchith29@users.noreply.github.com>
2024-07-08 14:23:12 +0530

    Final commit

---
+++ extract-aem-data/src/main/java/com/quipaem/extract/aem/data/service/AEMConnection.java
@@ -0,0 +1,36 @@
+package com.quipaem.extract.aem.data.service;
+
+import com.quipaem.extract.aem.data.utilities.Constants;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+
+@Service
+public class AEMConnection {
+
+    public String getConnection(URL url) throws IOException {
+
+        HttpURLConnection httpUrlconnection = (HttpURLConnection) url.openConnection();
+        httpUrlconnection.setRequestMethod("GET");
+
+        String credentials = Constants.USERNAME + ":" + Constants.PASSWORD;
+        String authHeaderValue = "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes());
+
+        httpUrlconnection.setRequestProperty("Authorization", authHeaderValue);
+        httpUrlconnection.setRequestProperty("Content-Type", "application/json");
+
+        if(httpUrlconnection.getResponseCode()==HttpURLConnection.HTTP_OK){
+            InputStreamReader inputStreamReader= new InputStreamReader(httpUrlconnection.getInputStream());
+            String data=FileCopyUtils.copyToString(inputStreamReader);
+            if (data.equals("{}")){
+                return null;
+            }
+            return data;
+        }
+        return null;
+    }
+}
---
+++ extract-aem-data/src/main/java/com/quipaem/extract/aem/data/utilities/Constants.java
@@ -0,0 +1,8 @@
+package com.quipaem.extract.aem.data.utilities;
+
+public class Constants {
+    public final static String USERNAME="quip-service-user";
+    public final static String PASSWORD="quipServiceUser";
+    public static String AEM_DATA_NAME= "seoPage";
+    public static String COLLECTION_NAME="seoPageData";
+}
